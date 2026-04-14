469c2697ce5d9ecfd369a5372af3d52f4d8a3da0
Huỳnh Đình Thịnh <118111790+HdThinh6102git@users.noreply.github.com>
2026-04-14 17:54:52 +0700

    fix: SP_RPA_DBG - handle extra data - DBG NEW CAR - Rule 6

--- BACKUP/SP_RPA_DBG_V1.0.5.sql
+++ BACKUP/SP_RPA_DBG_V1.0.5.sql
@@ -561,21 +561,8 @@ BEGIN
             SELECT COUNT(*) INTO v_log_after_rule4 FROM T_TEMP_RPA_DBG_PROCESSED;
             INSERT INTO T_RPA_DEBUG_LOG VALUES (IN_BATCH_ID, v_company_code, IN_INSURANCE_TYPE, IN_CONTRACT_TYPE, 'AFTER_RULE_4', v_log_after_rule4, NOW());
 
-            /* Rule 6: [납입방법]≠"월납,일시납"이면 원수사 원부확인하여 [보험료] 값수정 및 [납입방법]="일시납"으로 값수정 */
-            UPDATE T_TEMP_RPA_DBG_PROCESSED a
-            INNER JOIN T_RPA_INSURANCE_EXTRA_GUIDE b
-            ON
-                a.COLUMN_15 = b.SEARCH_DATA
-                AND b.SYS_FLAG = '1'
-                AND b.BATCH_ID = IN_BATCH_ID
-                AND b.COMPANY_CODE = v_company_code
-                AND b.INSURANCE_TYPE = IN_INSURANCE_TYPE
-                AND b.CONTRACT_TYPE = IN_CONTRACT_TYPE
-                AND b.BUSINESS_RULE_NO = 6
-                AND b.COLUMN_NAME = '보험료'
-                AND b.ACTION = 'UPD'
-            SET a.COLUMN_18 = b.AFTER_COLUMN_DATA,
-                a.COLUMN_22 = '일시납';
+            -- Rule 6: [납입방법]≠"월납,일시납"이면 원수사 원부확인하여 [보험료] 값수정 및 [납입방법]="일시납"으로 값수정
+            -- [PAUSE/SKIP] 원부확인 수동 처리 필요
 
         ELSEIF UPPER(IN_INSURANCE_TYPE) = 'GEN' AND UPPER(IN_CONTRACT_TYPE) = 'NEW' THEN
 
